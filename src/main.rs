4a42d674dea5b0198dc7aa0a0b6d654af2ca6abf
Ricky Ultimate <rickyogigi@gmail.com>
2024-08-07 07:48:51 +0100

    feat: define `add for addition of arrays

--- src/main.rs
+++ src/main.rs
@@ -21,10 +21,41 @@ impl Matrix {
             println!();
         }
     }
+
+    fn add(&self, other: &Matrix) -> Option<Matrix>{
+       if self.rows != other.rows && self.cols != other.cols{
+        return None;
+       }
+
+        let mut result = Matrix::new(self.rows, other.cols);
+
+        for i in 0..self.rows{
+            for j in 00..self.cols{
+                result.data[i][j] = self.data[i][j] + other.data[i][j];
+            }
+        }
+
+        Some(result)
+    }
+
 }
 
 fn main() {
     let mut matrix1 = Matrix::new(2, 3);
-    //matrix1.data = vec![vec![1, 2, 3], vec![4, 5, 6]];
+    matrix1.data = vec![vec![1, 2, 3], vec![4, 5, 6]];
+
+    let mut matrix2 = Matrix::new(2, 3);
+    matrix2.data = vec![vec![1, 2, 3], vec![4, 5, 7]];
+
+    println!("Matrix 1:");
     matrix1.display();
+    println!("\nMatrix 2:");
+    matrix2.display();
+
+    if let Some(result) = matrix1.add(&matrix2) {
+        println!("\nResult of addition:");
+        result.display();
+    } else {
+        println!("Matrices cannot be added");
+    }
 }
